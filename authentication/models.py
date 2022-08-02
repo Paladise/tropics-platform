e6d13b6711ebc3665029bc4992e54d396686201e
Paladise <andbas2015@gmail.com>
2022-08-02 18:16:42 -0400

    Add is_banned field for User

--- authentication/models.py
+++ authentication/models.py
@@ -6,28 +6,30 @@ from waters.models import Water
 
 class User(AbstractUser):
     id = models.AutoField(primary_key=True)
-
     middle_name = models.CharField(max_length=20, null=True, blank=True)
     full_name = models.CharField(max_length=50, null=False)
     graduation_year = models.IntegerField(default=0, null=False)
     is_teacher = models.BooleanField(default=False, null=False)
     is_student = models.BooleanField(default=True, null=False)
+    is_banned = models.BooleanField(default=False, null=False)
     gave_mangoes = models.ManyToManyField(Water, blank=True) # Blank for admin interface
 
     @property
-    def has_management_permission(self) -> bool:
+    def has_management_permission(self):
         return self.is_teacher or self.is_staff or self.is_superuser
+
 
     @property
     def short_name(self):
         return self.username
+
 
     @property
     def qualification(self):
         current_year = datetime.now().year
         res = self.graduation_year - current_year
 
-        if int(datetime.now().strftime('%m')) >= 7:
+        if int(datetime.now().strftime('%m')) >= 7: # Check if during/after July
             res -= 1
 
         if res < 0:
@@ -42,9 +44,9 @@ class User(AbstractUser):
             return "Freshman"
 
 
-
     def get_social_auth(self):
         return self.social_auth.get(provider="ion")
+
 
     def __str__(self):
         return self.short_name
